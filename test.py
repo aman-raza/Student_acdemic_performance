8141c831e42cbcc984589087c97092c8f2d75676
AMAN RAZA <53443872+aman-raza@users.noreply.github.com>
2020-09-17 16:44:25 +0530

    Update test.py
    Check is there any null value

--- test.py
+++ test.py
@@ -2,3 +2,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+student = pd.read_csv("student.csv")
+check = pd.isnull(student["AnnouncementsView"])
+check = pd.isnull(student["raisedhands"])
+check = pd.isnull(student["VisITedResources"])
+check = pd.isnull(student["Discussion"])
+student[check]
